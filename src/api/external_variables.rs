a4462b7a89102b1e0a70431363496c0b375d13e2
0xchase <chasekanipe@gmail.com>
2024-03-13 16:58:47 -0400

    updates

--- src/api/external_variables.rs
+++ src/api/external_variables.rs
@@ -1,1 +1,39 @@
-pub struct ExternalVariable;
+use serde_json::{json, Value};
+
+use crate::Type;
+
+pub struct ExternalVariable {
+    name: String,
+    ty: Value,
+    annotation: Value
+}
+
+impl ExternalVariable {
+    pub fn from_json(json: &Value) -> Self {
+        let name = json
+            .get("name")
+            .unwrap()
+            .to_string();
+
+        Self {
+            name: json["name"]
+                .as_str()
+                .unwrap()
+                .to_owned(),
+            ty: json["type"].clone(),
+            annotation: json["annotation"].clone()
+        }
+    }
+
+    pub fn to_json(&self) -> Value {
+        json!({
+            "name": self.name,
+            "type": self.ty,
+            "annotation": self.annotation
+        })
+    }
+}
+
+pub fn create_audio_file_object(frames: &Value, sample_rate: f64) -> Value {
+    todo!()
+}
